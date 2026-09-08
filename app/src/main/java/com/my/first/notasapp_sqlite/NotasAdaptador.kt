27d3f34e17d63bc5a2f10ab870e22c59aedf6968
C-Rodriguez-cmyk <rc.caleb18@gmail.com>
2026-09-08 07:54:50 -0600

    Adaptador

--- app/src/main/java/com/my/first/notasapp_sqlite/NotasAdaptador.kt
+++ app/src/main/java/com/my/first/notasapp_sqlite/NotasAdaptador.kt
@@ -1,17 +1,41 @@
 package com.my.first.notasapp_sqlite
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
 
 class NotasAdaptador (
     private var notas : List<Nota>,
-    context: Context) : RecyclerView.Adapter<NotasAdaptador.NotaViewHolder>{
+    context: Context) : RecyclerView.Adapter<NotasAdaptador.NotaViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotaViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_nota,
+            parent, false)
+        return NotaViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return notas.size
+    }
+
+    override fun onBindViewHolder(holder: NotaViewHolder, position: Int) {
+        val nota = notas[position]
+        holder.itemTitulo.text = nota.titulo
+        holder.itemDescripcion.text = nota.descripcion
+    }
 
     class NotaViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val itemTitulo : TextView = itemView.findViewById(R.id.item_titulo)
-        val itemDescripcion : TextView = itemView.find
+        val itemDescripcion : TextView = itemView.findViewById(R.id.item_descripcion)
     }
+
+    fun refrescarLista (nuevaNota : List<Nota>){
+        notas = nuevaNota
+        notifyDataSetChanged()
+    }
+
 }
